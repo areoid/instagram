a64ecacd2950ea6f5637e5f054ae3fb113acef66
areoid <punk.notok@gmail.com>
2016-04-25 13:31:02 +0700

    update curl proccess

--- Instagram.php
+++ Instagram.php
@@ -171,7 +171,13 @@ class Instagram
         }
 
         // execute the API and decoded the result
-        $response = api($url, [], 'get', '');
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 3);
+        $response = curl_exec($ch);
+        curl_close($ch);
         $response = json_decode($response);
 
         if(empty($response->data))
